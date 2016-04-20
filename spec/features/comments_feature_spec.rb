e080a186827c0c8b61773a9cecf814602ee1560f
Kyle Buttner <kylebuttner@gmail.com>
2016-04-20 12:21:25 +0100

    prevent users from editing other user comments

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -4,7 +4,8 @@ require 'helpers/user_management.rb'
 feature 'Comments' do
   before do
     user = create(:user)
-    Photo.create(image_file_name: 'test_image', user_id: user.id)
+    photo = Photo.create(image_file_name: 'test_image', user_id: user.id)
+    Comment.create(text: "This is not your comment", photo_id: photo.id, user_id: user.id)
   end
 
   scenario 'allows a user to leave a comment using a form' do
@@ -15,7 +16,7 @@ feature 'Comments' do
     click_button 'Post'
 
     expect(current_path).to eq photos_path
-    within("li") do
+    within("ul") do
       expect(page).to have_content 'Jealous'
       expect(page).to have_content 'test_user'
     end
@@ -27,11 +28,17 @@ feature 'Comments' do
     click_link 'Leave a comment'
     fill_in 'Text', with: 'Jealous'
     click_button 'Post'
-    p Comment.all
-    within("li") do
+    within("ul") do
       click_link 'Edit'
-      p current_path
     end
-    expect(current_path).to eq edit_comment_path(2)
+    expect(current_path).to eq edit_comment_path(4)
+  end
+
+  scenario 'don\'t allow a user to edit a comment that isn\'t theirs' do
+    sign_up
+    visit photos_path
+    within("ul") do
+      expect(page).not_to have_content "Edit"
+    end
   end
 end
